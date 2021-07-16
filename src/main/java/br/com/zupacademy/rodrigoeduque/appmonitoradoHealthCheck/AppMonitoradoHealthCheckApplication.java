89879c1da8216c46b80bd63c894ef9703470c400
Rodrigo Eustaquio Duque <rodrigoeduque@hotmail.com>
2021-07-16 09:29:00 -0300

    Projeto Criado

---
+++ src/main/java/br/com/zupacademy/rodrigoeduque/appmonitoradoHealthCheck/AppMonitoradoHealthCheckApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.rodrigoeduque.appmonitoradoHealthCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMonitoradoHealthCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMonitoradoHealthCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupacademy/rodrigoeduque/appmonitoradoHealthCheck/AppMonitoradoHealthCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.rodrigoeduque.appmonitoradoHealthCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMonitoradoHealthCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
